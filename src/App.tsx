2afcd9b15da194777bb2bbb5b5e137546c11b473
smartdev00 <gladwynanderson95@gmail.com>
2024-11-29 15:56:56 +0900

    feat: initial logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import { evaluation } from "./data/db";
+import { evaluation, personalProps } from "./data/db";
 import "./App.css";
 import Card from "./components/Card";
 import { CheckText } from "./components/CheckText";
@@ -7,28 +7,29 @@ import Modal from "./components/Modal";
 import { useState } from "react";
 
 function App() {
-  const [property, setProperty] = useState<string[]>([]);
+  // const [property, setProperty] = useState<object[]>([]);
+  const [modalData, setModalData] = useState<any>();
 
   return (
     <>
       <div>
         <p className='font-bold mb-4'>Start your free evaluation now</p>
         <div className='flex gap-4'>
-          {evaluation.map((eva) => {
+          {evaluation.items.map((eva) => {
             return (
               <Card
                 key={eva.title}
-                className={"border-4 relative w-56 " + eva.border.borderColor}
+                className={"border-4 relative w-56 " + eva.style.borderColor}
                 onClick={() => {
-                  setProperty(eva.properties);
+                  setModalData(eva.next);
                 }}>
-                <p className={`absolute top-0 left-0 px-1 text-sm py-0.5 ${eva.border.bgColor} ${eva.border.color}`}>
-                  {eva.border.title}
+                <p className={`absolute top-0 left-0 px-1 text-sm py-0.5 ${eva.style.bgColor} ${eva.style.color}`}>
+                  {eva.style.title}
                 </p>
                 <p>{eva.title}</p>
                 <img alt='face' src={eva.icon} />
                 <div>
-                  {eva.detail.map((det) => {
+                  {eva.checks.map((det) => {
                     return <CheckText key={det} text={det} />;
                   })}
                 </div>
@@ -36,11 +37,13 @@ function App() {
             );
           })}
         </div>
-        {property.length !== 0 && (
+        {modalData && (
           <Modal
             title='What type of property is it?'
+            data={modalData}
+            setData={setModalData}
             onClose={() => {
-              setProperty([]);
+              setModalData("");
             }}
           />
         )}
